039d130b915188cbcbfd9bd92b2d7fed155d80a2
Caio Maia <caaiomaia@gmail.com>
2022-08-02 19:25:06 -0300

    Configure tailwind css

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -14,16 +14,6 @@ export default (props: Props) => {
           <title>PoliData</title>
           <link rel="icon" href="/favicon.ico" />
           <meta charSet="utf-8"></meta>
-          <meta
-            name="viewport"
-            content="width=device-width, inicital-scale=1"
-          ></meta>
-          <link
-            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
-            rel="stylesheet"
-            integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
-            crossOrigin="anonymous"
-          />
         </Head>
 
         <div>
--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,7 @@ import Layout from "../components/Layout";
 export default function Home() {
   return (
     <Layout>
-      <div className="p-5 mb-4 bg-light rounded-3">
-        <div className="container-fluid py-5">
-          <h1 className="display-5 fw-bold">PoliData</h1>
-        </div>
-      </div>
+          <h1 className="text-3xl font-bold underline">PoliData</h1>
     </Layout>
   )
 }
