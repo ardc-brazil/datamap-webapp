7ba3a39a8b82e32c6c767b1a76b79df174b0ce0e
André Maia <andrenmaia@gmail.com>
2022-08-11 21:40:24 -0300

    Mobile version

--- pages/index.js
+++ pages/index.js
@@ -11,12 +11,13 @@ function TextDecorationBolder(props) {
 
 function BigButton(props) {
   return (
-    <Link href="/search">
-      <button className="self-center w-full h-full basis-6/12 
-      border border-secondary-900">
-        {props.children}
-      </button>
-    </Link>
+    <div className="self-center px-1">
+      <Link href="/search">
+        <button className="text-lg w-full h-full p-8 border border-secondary-900 bg-secondary-200 rounded-xl">
+          {props.children}
+        </button>
+      </Link>
+    </div>
   );
 }
 
@@ -25,7 +26,7 @@ export default function HomePage() {
     <Layout fluid={true}>
       <div className="special-background bg-fixed">
         <div className="container mx-auto flex flex-row flex-wrap py-20">
-          <div className="py-12 px-8 md:basis-6/12 bg-primary-50 drop-shadow-2xl">
+          <div className="py-12 px-8 md:basis-7/12 bg-primary-50 drop-shadow-2xl">
             <h1 className="md:display-2 sm:display-4 font-medium">
               Precisando analisar dados científicos? Use o&nbsp;
               <TextDecorationBolder>PoliData.</TextDecorationBolder>
@@ -35,28 +36,31 @@ export default function HomePage() {
         <div className="py-12 bg-gradient-to-t from-primary-50 h-40"></div>
       </div>
       <div className="container mx-auto">
-        <p className="display-4 my-20 text-center font-bold">
-          No PoliAnalytics você pode encontrar diversos dataset de diversas
-          fontes de dados do Brasil e do Mundo.
-        </p>
+        <section className="mb-40">
+          <p className="display-4 text-center font-bold">
+            No PoliAnalytics você pode encontrar diversos dataset de diversas
+            fontes de dados do Brasil e do Mundo.
+          </p>
+        </section>
+        <section className="mb-40">
+          <h1>
+            <TextDecorationBolder>Data Search</TextDecorationBolder>
+          </h1>
 
-        <h1>
-          <TextDecorationBolder>Data Search</TextDecorationBolder>
-        </h1>
+          <p className="text-xl">
+            Busque por categorias disponíveis ou faça uma busca avançada em
+            nossa ferramenta de Data Search.
+          </p>
 
-        <p>
-          Busque por categorias disponíveis ou faça uma busca avançada em nossa
-          ferramenta de Data Search.
-        </p>
-
-        <div class="flex gap-4 items-stretch h-64 p-8">
-          <BigButton>Aerosols</BigButton>
-          <BigButton>Atmospheric State</BigButton>
-          <BigButton>Cloud Properties</BigButton>
-          <BigButton>Radiometric</BigButton>
-          <BigButton>Surface Properties</BigButton>
-          <BigButton>Subsoil and groundwater properties</BigButton>
-        </div>
+          <div className="grid sm:grid-cols-4 grid-cols-1 gap-4 place-content-center py-4">
+            <BigButton>Aerosols</BigButton>
+            <BigButton>Atmospheric State</BigButton>
+            <BigButton>Cloud Properties</BigButton>
+            <BigButton>Radiometric</BigButton>
+            <BigButton>Surface Properties</BigButton>
+            <BigButton>Subsoil and groundwater properties</BigButton>
+          </div>
+        </section>
       </div>
     </Layout>
   );
