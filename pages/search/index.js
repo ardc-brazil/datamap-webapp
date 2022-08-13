658a2f32a761fd57f1051bc73a335bbc29b9c456
André Maia <andrenmaia@gmail.com>
2022-08-13 14:40:24 -0300

    Improve radio button

--- pages/search/index.js
+++ pages/search/index.js
@@ -2,21 +2,24 @@ import React from "react";
 
 import Layout from "../../components/Layout";
 
-function Checkbox() {
+function Checkbox(props) {
   return (
-    <div class="flex items-center mb-4">
-      <input
-        id="default-checkbox"
-        type="checkbox"
-        value=""
-        class="w-4 h-4 text-blue-600 bg-gray-100 rounded-lg border-gray-300 focus:ring-blue-500"
-      />
-      <label
-        for="default-checkbox"
-        class="ml-2 text-sm font-medium text-gray-900"
-      >
-        Default checkbox
-      </label>
+    <div className="flex items-center mb-4">
+      <div className="flex items-center mr-4">
+        <input
+          id={props.id}
+          type="radio"
+          value={props.value}
+          name="checkbox-component-"
+          className="w-5 h-5 accent-primary-900"
+        />
+        <label
+          for={props.id}
+          className="ml-2 text-sm font-medium text-primary-900 dark:text-primary-300 cursor-pointer"
+        >
+          {props.children}
+        </label>
+      </div>
     </div>
   );
 }
@@ -25,35 +28,52 @@ function FilterCriteria(props) {
   return (
     <div className="border-b-2 border-primary-200 mb-4">
       <h4>{props.title}</h4>
-      {props.children}
+      {props.options.map((x) => (
+        <Checkbox id={x.id} value={x.value} >{x.text}</Checkbox>
+      ))}
     </div>
   );
 }
 export default function SearchPage() {
+  const filterCriteria = [
+    {
+      id: 1,
+      title: "Category",
+      options: [
+        { id: 1, value: 1, text: "Category 1" },
+        { id: 2, value: 1, text: "Category 1" },
+        { id: 3, value: 1, text: "Category 1" },
+        { id: 4, value: 1, text: "Category 1" },
+      ],
+    },
+    {
+      id: 2,
+      title: "Datastreams",
+      options: [
+        { id: 5, value: 1, text: "Datastreams 1" },
+        { id: 6, value: 1, text: "Datastreams 1" },
+        { id: 7, value: 1, text: "Datastreams 1" },
+        { id: 8, value: 1, text: "Datastreams 1" },
+      ],
+    },
+  ];
+
   return (
     <Layout fluid={true}>
       <div className="grid grid-cols-12 gap-4">
         <div className="col-span-3 border-r-2 border-primary-200 px-4">
           <h3 className="underline decoration-primary-300">Filter By</h3>
 
-          <FilterCriteria title="Category">
-            <Checkbox></Checkbox>
-            <Checkbox></Checkbox>
-            <Checkbox></Checkbox>
-            <Checkbox></Checkbox>
-          </FilterCriteria>
-
-          <FilterCriteria title="Datastreams">
-            <Checkbox></Checkbox>
-            <Checkbox></Checkbox>
-            <Checkbox></Checkbox>
-            <Checkbox></Checkbox>
-          </FilterCriteria>
+          {filterCriteria.map((criteria) => (
+            <FilterCriteria
+              key={criteria.id}
+              title={criteria.title}
+              options={criteria.options}
+            ></FilterCriteria>
+          ))}
         </div>
 
-        <div className="col-span-9 px-4 ">
-          Resultado de busca
-        </div>
+        <div className="col-span-9 px-4 ">Resultado de busca</div>
       </div>
     </Layout>
   );
