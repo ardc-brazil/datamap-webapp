898318590f41aec8aa1f79ab8e3ef323f35d43f5
André Maia <andrenmaia@gmail.com>
2022-09-09 11:52:28 -0300

    Fix build

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,13 +1,14 @@
-
-describe('Navigation', () => {
-  it('should navigate to the search page', () => {
+describe("Navigation", () => {
+  it("should navigate to the search page", () => {
     // Start from the index page
-    cy.visit('http://localhost:3000/')
+    cy.visit("http://localhost:3000/");
 
     // Find a link with an href attribute containing "about" and click it
-    cy.get('#navbarItemSearch').click()
+    cy.get("#navbarItemSearch").click();
 
-  // The new url should include "/about"
-    cy.url().should('include', '/search')
-  })
-})
+    // The new url should include "/about"
+    cy.url().should("include", "/search");
+  });
+});
+
+export {};
