b48db4e106bf7c7c97fffa4ab5fe3e59e7f28119
André Maia <andrenmaia@gmail.com>
2022-09-13 10:31:29 -0300

    Add loading animation

--- components/DatasetDetails/TabPanelData.tsx
+++ components/DatasetDetails/TabPanelData.tsx
@@ -9,6 +9,44 @@ interface TabPanelDataObject {
   updateFrequency: string;
 }
 
+function LoadingAnimation() {
+  return (
+    <div role="status" className="space-y-2.5 animate-pulse max-w-lg mb-8">
+      <div className="flex items-center space-x-2 w-full">
+        <div className="h-2.5 bg-primary-200 rounded-full dark:bg-primary-700 w-32"></div>
+        <div className="h-2.5 bg-primary-300 rounded-full dark:bg-primary-600 w-24"></div>
+        <div className="h-2.5 bg-primary-300 rounded-full dark:bg-primary-600 w-full"></div>
+      </div>
+      <div className="flex items-center w-full space-x-2 max-w-[480px]">
+        <div className="h-2.5 bg-primary-200 rounded-full dark:bg-primary-700 w-full"></div>
+        <div className="h-2.5 bg-primary-300 rounded-full dark:bg-primary-600 w-full"></div>
+        <div className="h-2.5 bg-primary-300 rounded-full dark:bg-primary-600 w-24"></div>
+      </div>
+      <div className="flex items-center w-full space-x-2 max-w-[400px]">
+        <div className="h-2.5 bg-primary-300 rounded-full dark:bg-primary-600 w-full"></div>
+        <div className="h-2.5 bg-primary-200 rounded-full dark:bg-primary-700 w-80"></div>
+        <div className="h-2.5 bg-primary-300 rounded-full dark:bg-primary-600 w-full"></div>
+      </div>
+      <div className="flex items-center w-full space-x-2 max-w-[480px]">
+        <div className="h-2.5 bg-primary-200 rounded-full dark:bg-primary-700 w-full"></div>
+        <div className="h-2.5 bg-primary-300 rounded-full dark:bg-primary-600 w-full"></div>
+        <div className="h-2.5 bg-primary-300 rounded-full dark:bg-primary-600 w-24"></div>
+      </div>
+      <div className="flex items-center w-full space-x-2 max-w-[440px]">
+        <div className="h-2.5 bg-primary-300 rounded-full dark:bg-primary-600 w-32"></div>
+        <div className="h-2.5 bg-primary-300 rounded-full dark:bg-primary-600 w-24"></div>
+        <div className="h-2.5 bg-primary-200 rounded-full dark:bg-primary-700 w-full"></div>
+      </div>
+      <div className="flex items-center w-full space-x-2 max-w-[360px]">
+        <div className="h-2.5 bg-primary-300 rounded-full dark:bg-primary-600 w-full"></div>
+        <div className="h-2.5 bg-primary-200 rounded-full dark:bg-primary-700 w-80"></div>
+        <div className="h-2.5 bg-primary-300 rounded-full dark:bg-primary-600 w-full"></div>
+      </div>
+      <span className="sr-only">Loading...</span>
+    </div>
+  );
+}
+
 export function TabPanelData(props: TabPanelProps) {
   const [data, setData] = useState(null as TabPanelDataObject);
   const [isLoading, setLoading] = useState(false);
@@ -22,7 +60,7 @@ export function TabPanelData(props: TabPanelProps) {
         updateFrequency: "Quarterly",
       });
       setLoading(false);
-    }, 500);
+    }, 1500);
   }, []);
 
   const markdown = `# Ballaenarumque pellibus
@@ -109,7 +147,16 @@ A note[^1]
 * [ ] to do
 * [x] done`;
 
-  if (isLoading) return <p>Loading...</p>;
+  if (isLoading)
+    return (
+      <div className="min-h-screen">
+        <LoadingAnimation />
+        <LoadingAnimation />
+        <LoadingAnimation />
+        <LoadingAnimation />
+        <LoadingAnimation />
+      </div>
+    );
   if (!data) return <p>No dataset data</p>;
 
   return (
