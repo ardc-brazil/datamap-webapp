c73d7b7959ae02228df9545fcc49ee2f558f5d39
André Maia <andrenmaia@gmail.com>
2022-10-04 03:31:18 -0300

    Fill data from metadata search result

--- components/DatasetDetails/TabPanel.tsx
+++ components/DatasetDetails/TabPanel.tsx
@@ -3,6 +3,7 @@ import { Props } from "../../components/types/BaseInterfaces";
 
 export interface TabPanelProps extends Props {
   title: String;
+  dataset: any;
 }
 export function TabPanel(props: TabPanelProps) {
   return <div>{props.children}</div>;
--- pages/datasets/[datasetId].tsx
+++ pages/datasets/[datasetId].tsx
@@ -4,7 +4,8 @@ import { Tabs } from "../../components/DatasetDetails/Tabs";
 import { TabPanelData } from "../../components/DatasetDetails/TabPanelData";
 import { TabPanelMetadata } from "../../components/DatasetDetails/TabPanelMetadata";
 
-export default function DatasetDetails() {
+import Search from "../../lib/search";
+export default function DatasetDetails(props) {
   return (
     <Layout fluid={true}>
       <div className="">
@@ -12,25 +13,41 @@ export default function DatasetDetails() {
           <div className="flex flex-row py-8">
             {/* Title */}
             <div className="basis-10/12">
-              <h1 className="font-extrabold">Fullname for a Dataset</h1>
-              <p className="text-primary-500">
-                This is a subtitle it has to be between 20 and 80 characters
-                long.
-              </p>
+              <h1 className="font-extrabold">{props.dataset.name}</h1>
+              <p className="text-primary-500">{props.dataset.institution}</p>
             </div>
             {/* Actions */}
             <div className="">
-                <a className="btn-primary" href="/OCO2GriddedXCO2_20200727_v2_1605923534.nc" download>Download (1.8 MB)</a>
+              <a
+                className="btn-primary whitespace-nowrap"
+                href="/OCO2GriddedXCO2_20200727_v2_1605923534.nc"
+                download
+              >
+                Download (1.8 MB)
+              </a>
             </div>
           </div>
         </div>
         <div className="container mx-auto">
           <Tabs>
-            <TabPanelData title="Data" />
-            <TabPanelMetadata title="Metadata" />
+            <TabPanelData title="Data" dataset={props.dataset} />
+            <TabPanelMetadata title="Metadata" dataset={props.dataset} />
           </Tabs>
         </div>
       </div>
     </Layout>
   );
+}
+
+export async function getServerSideProps({ req, res, query }) {
+  const datasetId = query.datasetId as String;
+
+  const search = new Search();
+  const dataset = search.findOneBy(datasetId);
+
+  console.log(dataset);
+
+  return {
+    props: { dataset }, // will be passed to the page component as props
+  };
 }
