33f5065bacf52572ca67018879ed7274d955e561
André Maia <andre.maia@uber.com>
2023-07-21 02:33:00 -0300

    Fix position from profile page

--- components/Navbar/ActionItemsNavBar.tsx
+++ components/Navbar/ActionItemsNavBar.tsx
@@ -6,32 +6,30 @@ import AvatarButton from "../Profile/AvatarButton";
 export function ActionItemsNavBar() {
   const user = getUser();
   if (user) {
+    return <AvatarButton />;
+  } else {
     return (
-      <AvatarButton />
+      <>
+        <Link
+          href={{
+            pathname: "/account/login",
+            query: { phase: "sign-in" },
+          }}
+        >
+          <a href="#" className="btn-primary-outline mx-2">
+            Sign in
+          </a>
+        </Link>
+
+        <Link
+          href={{
+            pathname: "/account/login",
+            query: { phase: "register" },
+          }}
+        >
+          <button className="btn-primary">Register</button>
+        </Link>
+      </>
     );
   }
-
-  return (
-    <>
-      <Link
-        href={{
-          pathname: "/account/login",
-          query: { phase: "sign-in" },
-        }}
-      >
-        <a href="#" className="btn-primary-outline mx-2">
-          Sign in
-        </a>
-      </Link>
-
-      <Link
-        href={{
-          pathname: "/account/login",
-          query: { phase: "register" },
-        }}
-      >
-        <button className="btn-primary">Register</button>
-      </Link>
-    </>
-  );
 }
