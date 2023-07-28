cd770f69621d39b30f5dfce84b7d3188068f8b94
André Maia <andrenmaia@gmail.com>
2023-07-27 23:33:31 -0300

    Remove cypress e2e test

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,14 +1,14 @@
-describe("Navigation", () => {
-  it("should navigate to the search page", () => {
-    // Start from the index page
-    cy.visit("http://localhost:3000/");
+// describe("Navigation", () => {
+//   it("should navigate to the search page", () => {
+//     // Start from the index page
+//     cy.visit("http://localhost:3000/");
 
-    // Find a link with an href attribute containing "about" and click it
-    cy.get("#navbarItemSearch").click();
+//     // Find a link with an href attribute containing "about" and click it
+//     cy.get("#navbarItemSearch").click();
 
-    // The new url should include "/about"
-    cy.url().should("include", "/search");
-  });
-});
+//     // The new url should include "/about"
+//     cy.url().should("include", "/search");
+//   });
+// });
 
-export {};
+// export {};
