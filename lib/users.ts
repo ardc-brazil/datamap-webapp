c38ae99ccceeb4005b77e2ee5bdcd4c05ad262e3
André Maia <andrenmaia@gmail.com>
2023-07-30 20:28:07 -0300

    Append uid code to token jwt

---
+++ lib/users.ts
@@ -0,0 +1,16 @@
+
+export async function createUser(providerId: string) {
+    return {
+        providerId: "provider_id",
+        uid: "0cbb9610-4b81-44f0-8d1b-0da1fb4a4da7",
+        created_at: new Date()
+    };
+}
+
+export async function getUserBy(uid: string) {
+    return {
+        providerId: "provider_id",
+        uid: "0cbb9610-4b81-44f0-8d1b-0da1fb4a4da7",
+        created_at: new Date()
+    };
+}
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,6 +1,7 @@
 import NextAuth, { AuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import OrcidProvider from "../../../lib/OrcidOAuthProvider";
+import { createUser } from "../../../lib/users";
 
 export const authOptions: AuthOptions = {
   // Configure one or more authentication providers
@@ -16,11 +17,17 @@ export const authOptions: AuthOptions = {
   ],
   // debug: true,
   callbacks: {
-    async jwt({ token, account }) {
+    async jwt({ token, account, trigger }) {
       // Persist the OAuth access_token to the token right after signin
       if (account) {
         token.accessToken = account.access_token
       }
+
+      if (trigger == "signIn") {
+        const user = await createUser(token.email)
+        token.uid = user.uid
+      }
+
       return token
     },
     async session({ session, token, user }) {
@@ -35,6 +42,9 @@ export const authOptions: AuthOptions = {
     error: '/account/login?phase=sign-in', // Error code passed in query string as ?error=
     verifyRequest: '/', // (used for check email message)
     newUser: '/profile' // New users will be directed here on first sign in (leave the property out if not of interest)
+  },
+  session: {
+    strategy: "jwt"
   }
 }
 
