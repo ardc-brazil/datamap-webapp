bddef61cacf6a010ae070c7773001a89d2f0a6d0
André Maia <andrenmaia@gmail.com>
2023-08-06 22:19:30 -0300

    Fix LayoutFullScreen

--- components/LayoutFullScreen.tsx
+++ components/LayoutFullScreen.tsx
@@ -5,17 +5,16 @@ import CloseButton from './base/CloseButton';
 
 export default function LayoutFullScreen(props) {
   return (
-
-    <div className="absolute w-full top-0 z-50 h-screen left-0 bg-primary-50 flex flex-col justify-center items-center">
-      <div className="flex w-full h-20 px-4 py-4 items-center justify-start sticky top-0 border-b border-primary-200 bg-primary-50 scro">
+    <div className="absolute h-screen w-full top-0 z-50 left-0 bg-primary-50 flex flex-col justify-start overflow-hidden">
+      <div className="flex w-full h-20 px-4 py-4 items-center justify-start sticky top-0 border-b border-primary-200 bg-primary-50">
         <CloseButton onClick={() => Router.back()} />
       </div>
-      <div className="flex items-center justify-center w-full overflow-y-auto">
+      <div className="flex items-center justify-center w-full h-full overflow-y-auto">
         <div className="max-w-2xl h-full">
           {props.children[0]}
         </div>
       </div>
-      <div className="w-full h-24 border-t border-primary-200 sticky bottom-0 bg-primary-50 drop-shadow-lg shadow-primary-900">
+      <div className="w-full h-24 border-t border-primary-200 sticky bottom-0 bg-primary-50">
         {props.children[1]}
       </div>
     </div>
