143b5bfdc03e3992807b67c32e69faafd235dd9a
André Maia <andrenmaia@gmail.com>
2023-08-16 17:54:44 -0300

    Empty users api

--- pages/api/users.ts
+++ pages/api/users.ts
@@ -2,7 +2,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { createRouter } from "next-connect";
 import auth from "../../lib/auth";
-import { getUserBy } from "../../lib/users";
+// import { getUserBy } from "../../lib/users";
 import { ResponseError } from "../../types/ResponseError";
 
 const router = createRouter<NextApiRequest, NextApiResponse>();
@@ -10,8 +10,8 @@ const router = createRouter<NextApiRequest, NextApiResponse>();
 router
   .use(auth)
   .get((req, res) => {
-    const user = getUserBy("fake-uid-from-token-jwt");
-    res.json({ user });
+    // const user = getUserBy("fake-uid-from-token-jwt");
+    // res.json({ user });
   });
 
 export default router.handler({
