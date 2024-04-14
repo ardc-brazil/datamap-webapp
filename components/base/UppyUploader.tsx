f1cf3afd6269e695215e888fa883ff3610a8ba6b
André Maia <andrenmaia@gmail.com>
2024-04-14 20:03:25 -0300

    Fix env template

--- components/base/UppyUploader.tsx
+++ components/base/UppyUploader.tsx
@@ -10,32 +10,24 @@ export default function UppyUploader() {
     const { data: session } = useSession();
 
     // IMPORTANT: passing an initializer function to prevent Uppy from being reinstantiated on every render.
-    const [uppy] = useState(() => {
-
-        const instance = new Uppy({
+    const [uppy] = useState(
+        new Uppy({
             debug: false,
             autoProceed: true,
             meta: { "uid": "test1234" },
-
-        });
-
-        instance.on('complete', (result) => {
+        }).on('complete', (result) => {
             console.log('Upload result:', result)
         }).on('file-removed', (file, reason) => {
             if (reason === 'removed-by-user') {
                 // TODO: Implement call to remove file after uploaded.
             }
-        });
-
-        instance.use(Tus, {
+        }).use(Tus, {
             endpoint: process.env.NEXT_PUBLIC_TUS_SERVICE_ENDPOINT,
             headers: {
                 "X-User-Id": session?.user?.uid,
             }
-        });
-
-        return instance;
-    });
+        })
+    );
 
     // useEffect(() => {
     //     // Adding to global `meta` will add it to every file.
