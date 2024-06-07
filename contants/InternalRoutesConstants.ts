3580a65da08f49551b01e56da564bbc8239b0561
André Maia <andrenmaia@gmail.com>
2024-06-07 15:00:48 -0300

    Fix search page

--- contants/InternalRoutesConstants.ts
+++ contants/InternalRoutesConstants.ts
@@ -53,7 +53,7 @@ export const ROUTE_PAGE_PROFILE = ROUTE_APP_CONTEXT + '/profile';
  * Route to the search internal page.
  * @constant
  */
-export const ROUTE_PAGE_SEARCH = ROUTE_APP_CONTEXT + '/search';
+export const ROUTE_PAGE_SEARCH = ROUTE_APP_CONTEXT + '/datasets';
 
 /**
  * Route to the error handling internal page.
