4104f4c34a181479e96ec927da2c7496a7395e60
André Maia <andrenmaia@gmail.com>
2024-10-04 15:33:57 -0300

    Improve feature flag Ids organization

--- lib/featureFlags.ts
+++ lib/featureFlags.ts
@@ -1,22 +1,24 @@
 
+
+const engUserIds = [
+    
+    // André Maia
+    "afb36eb1-0018-4917-a115-350b5afd23e6",
+    "cbb0a683-630f-4b86-8b45-91b90a6fce1c",
+    "252e740f-02a5-4733-a592-b48bfdb4f6e7",
+    "64cd8613-a728-474b-b6c9-e366498edf53",
+
+    // Caio Maia
+    "e298c0ec-9ff2-4b35-9f8c-ea728a407a02",
+];
+
 /**
  * Is the feature Uppy upload enabled for user?
  * @param session NextAuth Session.
  * @returns True if the feature is enabled. Otherwise false.
  */
 export function isUppyUploadEnabled(session): boolean {
-
-    const usersEnabled = [
-        // André Maia
-        "cbb0a683-630f-4b86-8b45-91b90a6fce1c",
-        "252e740f-02a5-4733-a592-b48bfdb4f6e7",
-        "64cd8613-a728-474b-b6c9-e366498edf53",
-
-        // Caio
-        "e298c0ec-9ff2-4b35-9f8c-ea728a407a02",
-    ];
-
-    return usersEnabled.indexOf(session?.user?.uid) >= 0;
+    return engUserIds.indexOf(session?.user?.uid) >= 0;
 }
 
 
@@ -27,19 +29,7 @@ export function isUppyUploadEnabled(session): boolean {
  * @returns True if the feature is enabled. Otherwise false.
  */
 export function isDOIManagementEnabled(session): boolean {
-
-    const usersEnabled = [
-        // André Maia
-        "cbb0a683-630f-4b86-8b45-91b90a6fce1c",
-        "252e740f-02a5-4733-a592-b48bfdb4f6e7",
-        "64cd8613-a728-474b-b6c9-e366498edf53",
-        "1f7decd1-dfe6-42d2-a59d-9c638579c16f",
-
-        // Caio
-        "e298c0ec-9ff2-4b35-9f8c-ea728a407a02",
-    ];
-
-    return usersEnabled.indexOf(session?.user?.uid) >= 0;
+    return engUserIds.indexOf(session?.user?.uid) >= 0;
 }
 
 /**
@@ -48,19 +38,7 @@ export function isDOIManagementEnabled(session): boolean {
  * @returns True if the feature is enabled. Otherwise false.
  */
 export function isDOIUpdateStatusEnabled(session): boolean {
-
-    const usersEnabled = [
-        // André Maia
-        "cbb0a683-630f-4b86-8b45-91b90a6fce1c",
-        "252e740f-02a5-4733-a592-b48bfdb4f6e7",
-        "64cd8613-a728-474b-b6c9-e366498edf53",
-        "1f7decd1-dfe6-42d2-a59d-9c638579c16f",
-
-        // Caio
-        "e298c0ec-9ff2-4b35-9f8c-ea728a407a02",
-    ];
-
-    return usersEnabled.indexOf(session?.user?.uid) >= 0;
+    return engUserIds.indexOf(session?.user?.uid) >= 0;
 }
 
 /**
@@ -69,17 +47,5 @@ export function isDOIUpdateStatusEnabled(session): boolean {
  * @returns True if the feature is enabled. Otherwise false.
  */
 export function isNewVersionEnabled(session): boolean {
-
-    const usersEnabled = [
-        // André Maia
-        "cbb0a683-630f-4b86-8b45-91b90a6fce1c",
-        "252e740f-02a5-4733-a592-b48bfdb4f6e7",
-        "64cd8613-a728-474b-b6c9-e366498edf53",
-        "1f7decd1-dfe6-42d2-a59d-9c638579c16f",
-
-        // Caio
-        "e298c0ec-9ff2-4b35-9f8c-ea728a407a02",
-    ];
-
-    return usersEnabled.indexOf(session?.user?.uid) >= 0;
+    return engUserIds.indexOf(session?.user?.uid) >= 0;
 }
