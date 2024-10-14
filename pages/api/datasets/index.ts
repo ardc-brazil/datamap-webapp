1412d1a2390197c583eb1a48eaf83120c042d3f6
André Maia <andrenmaia@gmail.com>
2024-10-14 14:07:15 -0300

    Change dataset query to use the minimal parameter

--- pages/api/datasets/index.ts
+++ pages/api/datasets/index.ts
@@ -1,5 +1,4 @@
 
-import axios from "axios";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { createRouter } from "next-connect";
 import { NewContext } from "../../../lib/appLocalContext";
