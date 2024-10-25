159ec0a0ada757ae2dfafa6fc3c746b10a6765e7
André Maia <andrenmaia@gmail.com>
2024-10-25 11:34:10 -0300

    Remove e2e

--- .github/pull_request_template.md
+++ .github/pull_request_template.md
@@ -9,6 +9,7 @@ Describe
 
 ## 🧪 Playwright results
 Run `npx playwright test` and print the results here
+The project is https://github.com/ardc-brazil/datamap-e2e
 
 ## 📷 Screenshots 
 Update images
--- tests/app/main-manu.page.ts
+++
@@ -1,9 +0,0 @@
-import { type Locator, type Page } from '@playwright/test';
-
-export class MainMenuPage {
-    readonly getProfileMenuItem: Locator;
-
-    constructor(page: Page) {
-        this.getProfileMenuItem = page.getByRole('link', { name: 'Profile' });
-    }
-}
