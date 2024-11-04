9aab5a5dc3c61d14f2d6e6a82ac27f57f13c50f0
André Maia <andrenmaia@gmail.com>
2024-11-04 02:08:44 -0300

    Redirect to home after logout

--- pages/app/profile/index.tsx
+++ pages/app/profile/index.tsx
@@ -6,14 +6,14 @@ import LoggedLayout from "../../../components/LoggedLayout";
 import { signOut, useSession } from "next-auth/react";
 import { NewContext } from "../../../lib/appLocalContext";
 import { getUserByUID } from "../../../lib/users";
-import { ROUTE_PAGE_ERROR } from "../../../contants/InternalRoutesConstants";
+import { ROUTE_PAGE_ERROR, ROUTE_PAGE_HOME } from "../../../contants/InternalRoutesConstants";
 
 export default function ProfilePage(props) {
   const { data: session, status } = useSession();
 
   function clickSignOut() {
-    signOut().then((value) => {
-      Router.push("/");
+    signOut().finally(() => {
+      Router.push(ROUTE_PAGE_HOME)
     });
   }
 
