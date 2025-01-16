88577c7885eec180b428a4b533da2e1a513e16c2
André Maia <andrenmaia@gmail.com>
2025-01-16 09:34:13 -0300

    Enable DOI for some users

--- lib/featureFlags.ts
+++ lib/featureFlags.ts
@@ -12,6 +12,15 @@ const engUserIds = [
     "e298c0ec-9ff2-4b35-9f8c-ea728a407a02",
 ];
 
+const additionalUsers = [
+    // Aline Nascimento 
+    "39979429-1080-4eee-9a7d-3b6ced8ec252",
+    // Helvecio Bezerra Leal Neto
+    "4121ac26-f638-48e2-a433-624c89214bab",
+    // Alan James Peixoto Calheiros
+    "e92c32a7-78c6-415e-9411-fe0765d1fcae",
+]
+
 /**
  * Is the feature Uppy upload enabled for user?
  * @param session NextAuth Session.
@@ -27,5 +36,5 @@ export function isUppyUploadEnabled(session): boolean {
  * @returns True if the feature is enabled. Otherwise false.
  */
 export function isDOIUpdateStatusEnabled(session): boolean {
-    return engUserIds.indexOf(session?.user?.uid) >= 0;
+    return engUserIds.indexOf(session?.user?.uid) >= 0 || additionalUsers.indexOf(session?.user?.uid) >= 0;
 }
